b22a8ae32be501fd8dcf7327e639e21ee97f66fb
EstevaoKFZ <estevaofz@gmail.com>
2026-01-14 16:32:19 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.picpaysimplificado.repository;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User>findUserByDocumnet(String document);
+
+    Optional<User>findUserById(Long document);
+
+}
